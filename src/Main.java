44fd093acda1d9a7ac85dff6e09ec89296cb4a1e
Ruslan Sharipov <ruslan.sharipov.zokirovich@gmail.com>
2023-09-25 12:18:19 +0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    public static int MAXFLOR = 25;
+    public static void main(String[] args) {
+        Queue<String> queue = new ArrayDeque<>();
+
+        while (true) {
+            System.out.println("Ввод номеров этажей");
+            String input = scanner.nextLine();
+            if (Integer.parseInt(input) == 0) {
+                for (String s: queue) {
+                    System.out.print(s);
+                }
+                return;
+            }
+            String viv = "";
+            if (queue.isEmpty()) {
+                viv = " -> ";
+            } else {
+                viv = "";
+            }
+
+            if (Integer.parseInt(input) > MAXFLOR) {
+                System.out.println("Вы ввели неправильный этаж, попробуйте от 1 до 25");
+            } else {
+                queue.offer("Этаж " + input + viv);
+            }
+        }
+    }
+}
